3f7c9e218d6b702fa3087d6739062182ddae54b7
guechanlee <guechanlee@gmail.com>
2026-09-09 11:51:21 +0900

    20260909/selectUser

--- src/main/java/com/office/board/member/user/UserMemberDao.java
+++ src/main/java/com/office/board/member/user/UserMemberDao.java
@@ -1,12 +1,15 @@
 package com.office.board.member.user;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.office.board.member.IMemberDao;
+import com.office.library.user.member.UserMemberDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -42,9 +45,7 @@ public class UserMemberDao implements IMemberDao<UserMemberDto>{
 		return result;
 	}
 
-	/**
-	 * u_no에 해당하는 멤버의 u_phone 변경
-	 */
+	
 	@Override
 	public int updateMember(UserMemberDto memberDto) {
 		System.out.println(CLASS_NAME.concat("updateMember()"));
@@ -67,9 +68,7 @@ public class UserMemberDao implements IMemberDao<UserMemberDto>{
 		return result;
 	}
 	
-	/**
-	 * UserMemberDao에서는 사용할 필요 없음. 무조건 DB_CONNECTION_FAIL(-1)반환
-	 */
+	
 	@Override
 	public int updateMemberApproval(UserMemberDto userMemberDto) {
 		System.out.println(CLASS_NAME.concat("updateMemberApproval()"));
@@ -121,9 +120,7 @@ public class UserMemberDao implements IMemberDao<UserMemberDto>{
 		return userMemberDtos.size() > 0 ? userMemberDtos.get(0) : null;
 	}
 
-	/**
-	 * u_id가 %keyword%조건에 걸리는 멤버들 반환
-	 */
+
 	@Override
 	public List<UserMemberDto> selectMembersByKeywordOfId(String keyword) {
 		System.out.println(CLASS_NAME.concat("selectMembersByKeywordOfId()"));
@@ -186,5 +183,45 @@ public class UserMemberDao implements IMemberDao<UserMemberDto>{
 			printDto(dto);
 		}
 	}
+
+	
+	
+	public boolean isUserMember(String u_id) {
+		System.out.println(CLASS_NAME.concat("isUserMember()"));
+		
+		String sql =  "SELECT COUNT(*) FROM tbl_user "
+					+ "WHERE u_id = ?";
+		
+		int result = jdbcTemplate.queryForObject(sql, Integer.class, u_id);
+		
+		return result > 0 ? true : false;
+		
+	}
+
+
+	public UserMemberDto selectUser(String u_id) {
+		System.out.println(CLASS_NAME.concat("selectUser()"));
+		
+		String sql =  "SELECT * FROM tbl_user_member "
+					+ "WHERE u_m_id = ?";
+		
+		List<UserMemberDto> userMemberDtos = new ArrayList<UserMemberDto>();
+		
+		try {
+			
+			RowMapper<UserMemberDto> rowMapper = BeanPropertyRowMapper.newInstance(UserMemberDto.class);
+			userMemberDtos = jdbcTemplate.query(sql, rowMapper, u_id);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		
+		return userMemberDtos.size() > 0 ? userMemberDtos.get(0) : null;
+		
+	}
+	}
+
+	
 	
 }
