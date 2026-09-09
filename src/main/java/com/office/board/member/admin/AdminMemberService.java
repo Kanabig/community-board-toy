269b95a7c77f62293bf4ad5aeab78c9f6d4803f8
ho8566z <ho8566z@gmail.com>
2026-09-09 09:24:10 +0900

    0923 commit : service & dao trouble clear

--- src/main/java/com/office/board/member/admin/AdminMemberService.java
+++ src/main/java/com/office/board/member/admin/AdminMemberService.java
@@ -15,20 +15,22 @@ public class AdminMemberService {
 	final static public int ADMIN_ACCOUNT_CREATE_SUCEESS 	= 1;
 	final static public int ADMIN_ACCOUNT_CREATE_FAIL 		= -1;
 	
-//	final private AdminMemberDao adminMemberDao;
+	final private AdminMemberDao adminMemberDao;
 	final private PasswordEncoder passwordEncoder;
 	
 	
 	public int createAccountConfirm(AdminMemberDto adminMemberDto) {
 		System.out.println(CLASS_NAME.concat("createAccountConfirm()"));
 		
+		/*
 		boolean isMember = adminMemberDao.isAdminMember(adminMemberDto.getA_id());
 		System.out.println(CLASS_NAME.concat("isMember" + isMember));
 		
 		if (!isMember) {
 			String encodedPassword = passwordEncoder.encode(adminMemberDto.getA_pw());
 			adminMemberDto.setA_pw(encodedPassword);
-			
+			*/
+		
 			int result = adminMemberDao.insertMember(adminMemberDto);
 			
 			if (result > 0) {
@@ -40,18 +42,20 @@ public class AdminMemberService {
 				return ADMIN_ACCOUNT_CREATE_FAIL;
 				
 			}
+		/*	
 		} else {
 			System.out.println(CLASS_NAME.concat("ADMIN ACCOUNT CREATE FAIL"));
 			return ADMIN_ACCOUNT_ALREADY_EXIST;
 		
 		}
+		*/
 	}
 
 
 	public String loginConfirm(AdminMemberDto adminMemberDto) {
 		System.out.println(CLASS_NAME.concat("loginConfirm()"));
 		
-		AdminMemberDto selectAdminMemberDto = adminMemberDao.selectAdmin(adminMemberDto.getA_id());
+		AdminMemberDto selectAdminMemberDto = adminMemberDao.selectMember(adminMemberDto.getA_id());
 		
 		if (selectAdminMemberDto != null) {
 			
